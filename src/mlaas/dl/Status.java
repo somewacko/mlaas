2bebbb62f372821b866f65c4abcdb2ba169b609b
Trevor <Trevor@10.0.0.22>
2015-11-16 00:05:54 -0500

    Added constructor and getStatus() to Status

--- src/mlaas/dl/Status.java
+++ src/mlaas/dl/Status.java
@@ -2,4 +2,16 @@ package mlaas.dl;
 
 public class Status {
 
+	String thisStatus;
+	
+	public Status(String status)
+	{
+		thisStatus = status;
+	}
+
+	public String getStatus()
+	{
+		return thisStatus;
+	}
 }
+
